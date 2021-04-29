4d0f856f650053a6f9888fcdc0bf3f75535c635f
Triforze1 <b_farrill@ymail.com>
2021-04-28 23:12:37 -0400

    Almost Finished

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    [SerializeField] float timeToWait = 5f;
+
+    MeshRenderer meshRenderer;
+    Rigidbody rigidBody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+        
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > timeToWait)
+        {
+            meshRenderer.enabled = true;
+            rigidBody.useGravity = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Scorer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    int hits = -1;
+
+    private void OnCollisionEnter(Collision other) 
+    {
+
+        if (other.gameObject.tag != "Hit")
+        {
+            hits++;
+        }
+
+        Debug.Log($"You've bumped into a thing {hits} times");
+    }
+}
